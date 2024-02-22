a1936a65fd9de15c41a81a0ffd23c4937dfd7c8c
Marius Norheim <marius.norheim@azets.com>
2024-02-22 11:57:46 +0100

    Added post details component

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -7,12 +7,16 @@ import { BlogPostListComponent } from './blog-post-list/blog-post-list.component
 import { BlogService } from './blog.service';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { MenuModule } from 'primeng/menu';
+import { PanelModule } from 'primeng/panel';
 
 @NgModule({
     declarations: [BlogPostDetailsComponent, BlogPostListComponent],
     imports: [
         ButtonModule,
         CardModule,
+        MenuModule,
+        PanelModule,
         CommonModule,
         BlogRoutingModule,
     ],
