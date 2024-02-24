aa607e483428d8593b6c0738ebc038e7cfff830f
Marius Norheim <marius@mariusnorheim.dev>
2024-02-25 00:27:46 +0100

    Added active link class

--- src/app/features/navbar/navbar.component.ts
+++ src/app/features/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AvatarModule } from 'primeng/avatar';
@@ -9,6 +9,9 @@ import { DockModule } from 'primeng/dock';
 import { InputTextModule } from 'primeng/inputtext';
 import { ToolbarModule } from 'primeng/toolbar';
 import { MenuItem } from 'primeng/api';
+
+import { Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
     standalone: true,
@@ -24,38 +27,56 @@ import { MenuItem } from 'primeng/api';
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, OnDestroy {
     items: MenuItem[] | undefined;
-
-    constructor(private router: Router) {}
+    isActiveHomeLink = false;
+    private routerSub: Subscription;
+  
+    constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+      this.routerSub = this.router.events.pipe(
+        filter(event => event instanceof NavigationEnd),
+        map(() => this.activatedRoute)
+      ).subscribe(() => {
+        const currentRoute = this.router.url;
+        this.isActiveHomeLink = currentRoute === '/' || currentRoute.startsWith('/blog/');
+      });
+    }
 
     ngOnInit() {
         this.items = [
             {
                 label: 'Blog',
-                icon: 'pi pi-fw pi-book',
+                icon: 'pi-fw pi-book',
                 url: '/',
+                homelink: true,
                 external: false
             },
             {
                 label: 'Gallery',
-                icon: 'pi pi-fw pi-image',
+                icon: 'pi-fw pi-image',
                 url: '/gallery',
+                homelink: false,
                 external: false
             },
             {
                 label: 'Destinations',
-                icon: 'pi pi-fw pi-globe',
+                icon: 'pi-fw pi-globe',
                 url: '/destinations',
+                homelink: false,
                 external: false
             },
             {
                 label: 'Instagram',
-                icon: 'pi pi-fw pi-instagram',
+                icon: 'pi-fw pi-instagram',
                 url: 'https://www.instagram.com/mariusinbloom',
+                homelink: false,
                 external: true
             }
         ];
+    }
+
+    ngOnDestroy() {
+        this.routerSub.unsubscribe();
     }
 
     navigateTo(link: string): void {
