db87ef1526c21507b9af35dd119d0b9395296f35
Marius Norheim <marius@mariusnorheim.dev>
2024-02-25 17:27:39 +0100

    Adjusted gallery component

--- src/app/gallery/components/gallery-list/gallery-list.component.ts
+++ src/app/gallery/components/gallery-list/gallery-list.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit  } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { PaginatorState } from 'primeng/paginator';
+
 @Component({
   selector: 'app-gallery-list',
   templateUrl: './gallery-list.component.html',
   styleUrl: './gallery-list.component.css'
 })
 export class GalleryListComponent implements OnInit {
+    first: number = 0;
+    rows: number = 3;
+    totalGalleries: number = 2;
     items: { label?: string; icon?: string; separator?: boolean }[] = [];
 
     constructor(private route: ActivatedRoute) {}
@@ -29,5 +34,10 @@ export class GalleryListComponent implements OnInit {
                 icon: 'pi pi-times'
             }
         ];
+    }
+
+    onPageChange(event: PaginatorState) {
+        this.first = event.first ?? 0;
+        this.rows = event.rows ?? 4;
     }
 }
